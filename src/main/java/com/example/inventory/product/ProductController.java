81455cebf4924217668b0bfa7e81b5807bce0cee
Michał <michal.zyw0@gmail.com>
2023-02-18 15:35:31 +0100

    Create New Product

---
+++ src/main/java/com/example/inventory/product/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.inventory.product;
+
+
+import com.example.inventory.category.Category;
+import com.example.inventory.category.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepo;
+
+    @Autowired
+    private CategoryRepository categoryRepo;
+
+    @GetMapping("/products/new")
+    public String showNewProductForm(Model model){
+        List<Category> listCategories = categoryRepo.findAll();
+        model.addAttribute("product", new Product());
+        model.addAttribute("listCategories", listCategories);
+        return "product_form";
+    }
+
+    @PostMapping("/products/save")
+    public String saveProduct(Product product){
+        productRepo.save(product);
+
+        return "redirect:/";
+    }
+}
