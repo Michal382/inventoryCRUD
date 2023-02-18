f1d44a495861be9044935b8ac6e221d50e2345a0
Michał <michal.zyw0@gmail.com>
2023-02-18 14:24:30 +0100

    Initial commit

---
+++ src/main/java/com/example/inventory/category/Category.java
@@ -0,0 +1,29 @@
+package com.example.inventory.category;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
